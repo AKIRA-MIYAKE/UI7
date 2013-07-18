938197e027dce6534e0c919ce540ce6c4506136e
AKIRA-MIYAKE <akira.aktk.miyake@gmail.com>
2013-07-19 00:05:47 +0900

    update

--- javascripts/lib/pagetransition.js
+++ javascripts/lib/pagetransition.js
@@ -1,35 +1,31 @@
 (function($) {
-  
-  $.extend({
-    pageTransition: function() {
-                
-      var running = function(options) {
-        var $in = options.in;
-        var $out = options.out;
-        var inEffect = options.inEffect || 'pt-page-moveFromRight';
-        var outEffect = options.outEffect || 'pt-page-moveToLeft';
-        
-        var effectEndEventNames = {
-          'WebkitAnimation' : 'webkitAnimationEnd',
-          'OAnimation' : 'oAnimationEnd',
-          'msAnimation' : 'MSAnimationEnd',
-          'animation' : 'animationend'
-        };
-        var effectEndEventName = effectEndEventNames[Modernizr.prefixed('animation')];
-                
-        $in.addClass(inEffect).addClass('page-current').on(effectEndEventName, function() {
-          $in.off(effectEndEventName);
-          $in.removeClass(inEffect);
-        });
-        
-        $out.addClass(outEffect).on(effectEndEventName, function() {
-          $out.off(effectEndEventName);
-          $out.removeClass(outEffect).removeClass('page-current');
-        });
-      };
-      
-      return running;
-    }
-  });
-    
+	
+	$.extend({
+		pageTransition: function(options) {
+			var $in = options.in;
+			var $out = options.out;
+			var inEffect = options.inEffect || 'pt-page-moveFromRight';
+			var outEffect = options.outEffect || 'pt-page-moveToLeft';
+			
+			var effectEndEventNames = {
+  	  	'WebkitAnimation' : 'webkitAnimationEnd',
+    		'OAnimation' : 'oAnimationEnd',
+ 	   		'msAnimation' : 'MSAnimationEnd',
+				'animation' : 'animationend'
+    	};
+    	
+    	var effectEndEventName = effectEndEventNames[Modernizr.prefixed('animation')];
+		
+			$in.addClass(inEffect).addClass('page-current').on(effectEndEventName, function() {
+    		$in.off(effectEndEventName);
+      	$in.removeClass(inEffect);
+    	});
+    	
+    	$out.addClass(outEffect).on(effectEndEventName, function() {
+      	$out.off(effectEndEventName);
+      	$out.removeClass(outEffect).removeClass('page-current');
+    	});
+		}
+	});
+	
 })(jQuery);
